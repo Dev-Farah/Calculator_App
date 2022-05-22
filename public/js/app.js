d9dbcbb190f5d476fddaf3d01a5112c9600446f1
FarahSyed <farahsyed44@gmail.com>
2022-05-22 02:19:07 -0800

    add bubbles animation

--- public/js/app.js
+++ public/js/app.js
@@ -21,3 +21,23 @@ function backspace() {
 // Button Sound Effect
 const audio = new Audio();
 audio.src = "./images/pop.mp3";
+
+
+// Bubbles Animation
+function createBubble() {
+    let section = document.querySelector('section');
+    let createElement = document.createElement('span');
+    var size = Math.random() * 60;
+
+    createElement.style.width = 20 + size + 'px';
+    createElement.style.height = 20 + size + 'px';
+
+    createElement.style.left = Math.random() * innerWidth + 'px';
+    section.appendChild(createElement);
+
+    setTimeout(() => {
+        createElement.remove();
+    }, 3000)
+
+}
+setInterval(createBubble,50);
